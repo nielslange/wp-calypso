523d8f415bec843bc7cc86849f2021d2df7e64dc
ramonjd <ramonjd@gmail.com>
2018-12-03 17:18:42 +1100

    Added basic component skeleton, including form options list. Added the step to the onboarding dev flow. Hooking in the state methods.
    Sending themeRepo as a dependency
    Added submit method
    
    Added reducer to bundle.
    Fixed translate function typo.
    Added default theme slug to component state.
    
    Updating the site mock up component when the user selects a new style

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -401,6 +401,11 @@ export function generateSteps( {
 			stepName: 'site-information',
 			providesDependencies: [ 'siteTitle', 'address', 'email', 'phone' ],
 		},
+
+		'site-style': {
+			stepName: 'site-style',
+			providesDependencies: [ 'themeSlugWithRepo' ],
+		},
 	};
 }
 
