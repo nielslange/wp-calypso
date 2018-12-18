19d595cb893e6bf094d8d29c52196b170d4fd3b8
ramonjd <ramonjd@gmail.com>
2018-12-18 18:13:33 +1100

    This is to ensure that the free text search doesn't bork with any regex lookup of the suggestion list.

--- client/components/suggestion-search/index.jsx
+++ client/components/suggestion-search/index.jsx
@@ -5,7 +5,7 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { noop } from 'lodash';
+import { escapeRegExp, noop } from 'lodash';
 
 /**
  * Internal dependencies
@@ -49,7 +49,6 @@ class SuggestionSearch extends Component {
 
 	handleSuggestionChangeEvent = ( { target: { value } } ) => {
 		this.setState( { query: value, inputValue: value } );
-
 		this.props.onChange( value );
 	};
 
@@ -139,7 +138,7 @@ class SuggestionSearch extends Component {
 				/>
 				<Suggestions
 					ref={ this.setSuggestionsRef }
-					query={ this.state.query }
+					query={ escapeRegExp( this.state.query ) }
 					suggestions={ this.getSuggestions() }
 					suggest={ this.handleSuggestionMouseDown }
 				/>
